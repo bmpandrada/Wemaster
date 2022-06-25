a684df1f30252e3099b4ad699e03efb0e1a374a8
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2022-06-25 18:20:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+$(document).ready(function () {
+  //THIS IS FOR MENU TOGGLE
+  //MENU TAB WILL APPEAR AFTER MINIMIZING
+  $(".menu-btn").click(function () {
+    $(this).toggleClass("active");
+
+    $(".navbar .menu").toggleClass("active");
+  });
+
+  //THIS IS FOR AFTER CLICK MENU//
+  $(".navbar .menu").on("click", (event) => {
+    $(event.currentTarget).toggleClass("active");
+    $(this).find(".menu-btn").toggleClass("active");
+  });
+
+  $(window).scroll(function () {
+    //-------------------------///
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+});
